c6bc4c73ec789b7ec41ee0c09b97053e3c395aaf
Jordan Jantschulev <j.jantschulev@gmail.com>
2022-11-12 22:21:38 +1100

    added font-awesome

--- app/[company]/page.tsx
+++ app/[company]/page.tsx
@@ -1,9 +1,25 @@
 import { PageProps } from "@/lib/util";
+import { faRocket } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default async function CompanyPage({}: PageProps) {
   return (
     <div className="p-4 border-2 border-slate-800 rounded-lg">
-      <h1 className="text-2xl font-bold">List of courses here</h1>
+      <h1 className="text-2xl text-center font-bold">List of courses here</h1>
+      <div className="flex justify-center gap-4 p-4">
+        <FontAwesomeIcon
+          className="text-4xl p-4 hover:bg-emerald-600 hover:text-slate-900 rounded-full transition-all"
+          icon={faRocket}
+        />
+        <FontAwesomeIcon
+          className="text-4xl p-4 hover:bg-amber-600 hover:text-slate-900 rounded-full transition-all"
+          icon={faRocket}
+        />
+        <FontAwesomeIcon
+          className="text-4xl p-4 hover:bg-red-600 hover:text-slate-900 rounded-full transition-all"
+          icon={faRocket}
+        />
+      </div>
     </div>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,7 @@
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
 import "./globals.css";
+config.autoAddCss = false;
 
 export default function RootLayout({
   children,
