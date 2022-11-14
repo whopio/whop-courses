bc254dd6e2ce7bd91b01f527e9d495245701c862
Jordan Jantschulev <j.jantschulev@gmail.com>
2022-11-15 08:45:40 +1100

    removed package

--- lib/util.ts
+++ lib/util.ts
@@ -1,14 +1,5 @@
 import { ReactNode } from "react";
 
-export const fetcher = (...args: any[]) =>
-  // @ts-ignore
-  fetch(...args).then(async (res) => {
-    if (res.ok) {
-      return res.json();
-    }
-    throw new Error(res.statusText);
-  });
-
 export type PageProps = {
   params?: Record<string, string>;
   searchParams?: Record<string, string>;
