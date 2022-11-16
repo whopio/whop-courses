d512494f6b2363c4ebdf147e0b0613f652028fc0
Jordan Jantschulev <j.jantschulev@gmail.com>
2022-11-16 13:04:53 +1100

    use company route instead of tag

--- app/[company]/page.tsx
+++ app/[company]/page.tsx
@@ -28,7 +28,7 @@ export default async function CompanyPage({ params }: PageProps) {
       <h3 className="text-xl font-bold">Your on-going courses</h3>
       <div className="flex flex-wrap gap-4">
         <CourseCard
-          companyId={company.tag}
+          companyId={company.route}
           courseId="course-0"
           image={
             "https://images.unsplash.com/photo-1666624481302-3a9920b039b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDR8NnNNVmpUTFNrZVF8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
@@ -37,7 +37,7 @@ export default async function CompanyPage({ params }: PageProps) {
           subtitle={"Lorem ipsum dolor sit amet, consectetur adipiscing elit."}
         />
         <CourseCard
-          companyId={company.tag}
+          companyId={company.route}
           courseId="course-1"
           image={
             "https://images.unsplash.com/photo-1666797630713-f5a2e54d3d23?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDZ8NnNNVmpUTFNrZVF8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
@@ -49,7 +49,7 @@ export default async function CompanyPage({ params }: PageProps) {
       <h3 className="text-xl font-bold">Other Courses</h3>
       <div className="flex flex-wrap gap-4">
         <CourseCard
-          companyId={company.tag}
+          companyId={company.route}
           courseId="course-2"
           image={
             "https://images.unsplash.com/photo-1656498933204-93bbef61edeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDE1fDZzTVZqVExTa2VRfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
