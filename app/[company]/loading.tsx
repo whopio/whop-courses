0e1eec32a783db23f4801e61c3cf5faf36c7c380
Jordan Jantschulev <j.jantschulev@gmail.com>
2022-11-30 13:30:23 +1100

    lighted loading bars

--- app/[company]/loading.tsx
+++ app/[company]/loading.tsx
@@ -1,23 +1,17 @@
 export default function CompanyLoading() {
   return (
     <div className="p-8 flex flex-col gap-6 m-auto max-w-7xl">
-      <div className="rounded-lg w-full h-80 bg-neutral-400 animate-pulse" />
+      <div className="rounded-lg w-full h-80 bg-neutral-200 animate-pulse" />
       <div className="flex gap-2 items-center">
         <h1 className="text-3xl font-bold">Welcome to</h1>
-        <div className="h-10 w-48 rounded-lg bg-neutral-400 animate-pulse" />
+        <div className="h-10 w-48 rounded-lg bg-neutral-200 animate-pulse" />
       </div>
       <div className="flex flex-col gap-1">
-        <div className="h-5 w-full rounded-lg animate-pulse bg-neutral-400"></div>
-        <div className="h-5 w-full rounded-lg animate-pulse bg-neutral-400"></div>
-        <div className="h-5 w-full rounded-lg animate-pulse bg-neutral-400"></div>
-        <div className="h-5 w-full rounded-lg animate-pulse bg-neutral-400"></div>
-        <div className="h-5 w-full rounded-lg animate-pulse bg-neutral-400"></div>
-        <div className="h-5 w-2/3 rounded-lg animate-pulse bg-neutral-400"></div>
-      </div>
-      <h3 className="text-xl font-bold">Your on-going courses</h3>
-      <div className="flex flex-wrap gap-4">
-        <CourseCardSkeleton />
-        <CourseCardSkeleton />
+        <div className="h-5 w-full rounded-lg animate-pulse bg-neutral-200"></div>
+        <div className="h-5 w-full rounded-lg animate-pulse bg-neutral-200"></div>
+        <div className="h-5 w-full rounded-lg animate-pulse bg-neutral-200"></div>
+        <div className="h-5 w-full rounded-lg animate-pulse bg-neutral-200"></div>
+        <div className="h-5 w-2/3 rounded-lg animate-pulse bg-neutral-200"></div>
       </div>
       <h3 className="text-xl font-bold">Other Courses</h3>
       <div className="flex flex-wrap gap-4">
@@ -29,12 +23,12 @@ export default function CompanyLoading() {
   );
 }
 
-function CourseCardSkeleton() {
+export function CourseCardSkeleton() {
   return (
     <div className="w-80 flex flex-col gap-2 transition rounded-lg  group cursor-pointer">
-      <div className="w-full aspect-video rounded-lg animate-pulse bg-neutral-400"></div>
-      <div className="h-6 w-4/5 rounded-lg animate-pulse bg-neutral-400"></div>
-      <div className="h-4 w-2/3 rounded-lg animate-pulse bg-neutral-400"></div>
+      <div className="w-full aspect-video rounded-lg animate-pulse bg-neutral-200"></div>
+      <div className="h-6 w-4/5 rounded-lg animate-pulse bg-neutral-200"></div>
+      <div className="h-4 w-2/3 rounded-lg animate-pulse bg-neutral-200"></div>
     </div>
   );
 }
--- app/loading.tsx
+++ app/loading.tsx
@@ -2,9 +2,10 @@ import { Button } from "@/ui/Button";
 import {
   faArrowRightFromBracket,
   faCaretDown,
-  faLockOpen,
+  faLinesLeaning,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { CourseCardSkeleton } from "./[company]/loading";
 
 export default function CompanyLoading() {
   return (
@@ -24,19 +25,19 @@ export default function CompanyLoading() {
 
         <div className="flex-1 flex flex-col gap-2">
           <div className=" bg-slate-800 hover:bg-slate-700 p-4 rounded-lg flex items-center text-slate-50 transition">
-            <FontAwesomeIcon icon={faLockOpen} className="w-10" />
+            <FontAwesomeIcon icon={faLinesLeaning} className="w-10" />
             <div className="h-6 flex-1 rounded-lg bg-neutral-700 animate-pulse"></div>
           </div>
           <div className=" bg-slate-800 hover:bg-slate-700 p-4 rounded-lg flex items-center text-slate-50 transition">
-            <FontAwesomeIcon icon={faLockOpen} className="w-10" />
+            <FontAwesomeIcon icon={faLinesLeaning} className="w-10" />
             <div className="h-6 flex-1 rounded-lg bg-neutral-700 animate-pulse"></div>
           </div>
           <div className=" bg-slate-800 hover:bg-slate-700 p-4 rounded-lg flex items-center text-slate-50 transition">
-            <FontAwesomeIcon icon={faLockOpen} className="w-10" />
+            <FontAwesomeIcon icon={faLinesLeaning} className="w-10" />
             <div className="h-6 flex-1 rounded-lg bg-neutral-700 animate-pulse"></div>
           </div>
           <div className=" bg-slate-800 hover:bg-slate-700 p-4 rounded-lg flex items-center text-slate-50 transition">
-            <FontAwesomeIcon icon={faLockOpen} className="w-10" />
+            <FontAwesomeIcon icon={faLinesLeaning} className="w-10" />
             <div className="h-6 flex-1 rounded-lg bg-neutral-700 animate-pulse"></div>
           </div>
         </div>
@@ -54,23 +55,17 @@ export default function CompanyLoading() {
 
       <div className="flex-1 overflow-auto">
         <div className="p-8 flex flex-col gap-6 m-auto max-w-7xl">
-          <div className="rounded-lg w-full h-80 bg-neutral-400 animate-pulse" />
+          <div className="rounded-lg w-full h-80 bg-neutral-200 animate-pulse" />
           <div className="flex gap-2 items-center">
             <h1 className="text-3xl font-bold">Welcome to</h1>
-            <div className="h-10 w-48 rounded-lg bg-neutral-400 animate-pulse" />
+            <div className="h-10 w-48 rounded-lg bg-neutral-200 animate-pulse" />
           </div>
           <div className="flex flex-col gap-1">
-            <div className="h-5 w-full rounded-lg animate-pulse bg-neutral-400"></div>
-            <div className="h-5 w-full rounded-lg animate-pulse bg-neutral-400"></div>
-            <div className="h-5 w-full rounded-lg animate-pulse bg-neutral-400"></div>
-            <div className="h-5 w-full rounded-lg animate-pulse bg-neutral-400"></div>
-            <div className="h-5 w-full rounded-lg animate-pulse bg-neutral-400"></div>
-            <div className="h-5 w-2/3 rounded-lg animate-pulse bg-neutral-400"></div>
-          </div>
-          <h3 className="text-xl font-bold">Your on-going courses</h3>
-          <div className="flex flex-wrap gap-4">
-            <CourseCardSkeleton />
-            <CourseCardSkeleton />
+            <div className="h-5 w-full rounded-lg animate-pulse bg-neutral-200"></div>
+            <div className="h-5 w-full rounded-lg animate-pulse bg-neutral-200"></div>
+            <div className="h-5 w-full rounded-lg animate-pulse bg-neutral-200"></div>
+            <div className="h-5 w-full rounded-lg animate-pulse bg-neutral-200"></div>
+            <div className="h-5 w-2/3 rounded-lg animate-pulse bg-neutral-200"></div>
           </div>
           <h3 className="text-xl font-bold">Other Courses</h3>
           <div className="flex flex-wrap gap-4">
@@ -80,16 +75,6 @@ export default function CompanyLoading() {
           </div>
         </div>
       </div>
-    </div>
-  );
-}
-
-function CourseCardSkeleton() {
-  return (
-    <div className="w-80 flex flex-col gap-2 transition rounded-lg  group cursor-pointer">
-      <div className="w-full aspect-video rounded-lg animate-pulse bg-neutral-400"></div>
-      <div className="h-6 w-4/5 rounded-lg animate-pulse bg-neutral-400"></div>
-      <div className="h-4 w-2/3 rounded-lg animate-pulse bg-neutral-400"></div>
     </div>
   );
 }
